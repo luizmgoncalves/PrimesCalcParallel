2470e18393f268c1294d2ef9a1fa9d70b82cb4bc
Luiz Miguel dos Santos Gonçalves <lm4academy@gmail.com>
2023-07-06 15:41:35 -0300

    memory leaking fixed

--- parallelv1.cpp
+++ parallelv1.cpp
@@ -107,6 +107,7 @@ int main(int argc, char *argv[])
                     // cout << *it << endl;
                     PRIMES[curr_i++] = *it;
                 }
+                delete primes_computed;
             }
         }
         if (prox == limit)
